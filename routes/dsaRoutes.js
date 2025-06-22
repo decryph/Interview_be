985eacfffa8ca685d1d36788b09cd6c6c13537d8
Shruti <158175549+decryph@users.noreply.github.com>
2025-06-22 15:02:38 +0530

    Update dsaRoutes.js

--- routes/dsaRoutes.js
+++ routes/dsaRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { generateQuestion, submitCode, compileCode, getHistory, runCustom } = require("../controllers/dsacontroller");
+const { generateQuestion, submitCode, compileCode, getHistory } = require("../controllers/dsacontroller");
 
 router.post("/generate-question", generateQuestion);
 router.post("/submit-code", submitCode);
 router.post("/compile-code", compileCode);
 router.get("/history",getHistory);
-router.post("/api/dsa/run-custom", runCustom)
 
 
 module.exports = router;
