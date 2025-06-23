1d8d9f0ad8306cbde602d3e8b7a357f0d67f2c93
Shruti <158175549+decryph@users.noreply.github.com>
2025-06-23 20:57:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const voiceRoutes = require("../routes/voiceRoutes");
-app.use("/api/voice", voiceRoutes);
-
 const geminiRoutes = require("./routes/geminiFeedbackRoutes");
 app.use("/api", geminiRoutes);
 
