9dc268e2e8863dbba80720bbfe2597bdb5ae6581
Shruti <158175549+decryph@users.noreply.github.com>
2025-06-23 19:12:01 +0530

    Delete routes/submission.js

--- routes/submission.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { submitCode, getHistory } = require('../controllers/dsacontroller');
-const auth = require('../middleware/auth');
-router.post('/', auth, submitCode);
-router.get('/', auth, getHistory); // returns logged-in user's submissions
-
-module.exports = router;
