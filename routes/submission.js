ac3f94c934dcd6c595208f0bc3cfa25d95eaeabc
Shruti <158175549+decryph@users.noreply.github.com>
2025-06-23 18:46:00 +0530

    Update submission.js

--- routes/submission.js
+++ routes/submission.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { submitCode, getHistory } = require('../controllers/dsaController');
+const { submitCode, getHistory } = require('../controllers/dsacontroller');
 const auth = require('../middleware/auth');
 router.post('/', auth, submitCode);
 router.get('/', auth, getHistory); // returns logged-in user's submissions
