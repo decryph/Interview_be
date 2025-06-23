e9cbe6a051b89d9d436d68b750bbe845f6e170e2
Shruti <158175549+decryph@users.noreply.github.com>
2025-06-23 22:44:38 +0530

    Update voiceRoutes.js

--- routes/voiceRoutes.js
+++ routes/voiceRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
+
 const voiceController = require("../controllers/voiceController");
 
+const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 
 router.post("/process", upload.single("audio"), voiceController.processVoiceAnswer);
