a649528b160abf9a581ced495de811fcd21ba14d
Shruti <158175549+decryph@users.noreply.github.com>
2025-06-23 18:42:18 +0530

    Update Submission.js

--- models/Submission.js
+++ models/Submission.js
@@ -4,7 +4,7 @@ const SubmissionSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: false
+    required: true
   },
   question: String,
   code: String,
